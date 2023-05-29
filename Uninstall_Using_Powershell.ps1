8926aea206363e142646c02e90452ff63c071dbd
dCloser <oghenefejiroc@gmail.com>
2023-05-28 22:09:57 -0500

    Update Uninstall_Using_Powershell.ps1

--- Uninstall_Using_Powershell.ps1
+++ Uninstall_Using_Powershell.ps1
@@ -41,14 +41,3 @@ Invoke-WebRequest -Uri $DownloadUrl -OutFile “$env:TEMP\$( Split-Path -Path $D
 Write-Host “Installing the latest Notepad++”
 Start-Process -FilePath “$env:TEMP\$( Split-Path -Path $DownloadUrl -Leaf )” -ArgumentList “/S” -Wait
 }
-
-$w64=Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | where-Object DisplayName -like ‘NotePad++*’
-$w32=Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | where-Object DisplayName -like ‘NotePad++*’
-Write-Output “After::::”
-if ($w64) {
-write-Host $w64.DisplayVersion
-} elseif ($w32) {
-write-Host $w32.DisplayVersion
-} else {
-Write-Output “No Version Found”
-}
